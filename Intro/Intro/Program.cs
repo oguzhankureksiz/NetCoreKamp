09c4f5dbdb24a3bf5e49f44468e00b5c85843b41
Oğuzhan Küreksiz <kureksizoguzhan@gmail.com>
2024-03-16 00:07:02 +0300

    1.gün tamamlandı.

--- Intro/Intro/Program.cs
+++ Intro/Intro/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
 
 string message1 = "Krediler";
 int term = 12;
@@ -22,7 +25,14 @@ string[] loans = {"Kredi 1","Kredi 2", "Kredi 3", "Kredi 4", "Kredi 5", "Kredi 6
 
 for (int i = 0; i < loans.Length; i++)
 {
-    Console.WriteLine(loans[0]);
+    Console.WriteLine(loans[i]);
+}
+
+CourseManager courseManager = new();
+Course[] courses = courseManager.GetAll();
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
 }
 
 Console.WriteLine("Kod bitti.");
