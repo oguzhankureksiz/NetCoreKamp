7420b7e88da420444e041067d994075d6617836b
Oğuzhan Küreksiz <kureksizoguzhan@gmail.com>
2024-03-18 16:27:06 +0300

    2. gün tamamlandı.

--- Intro/Intro/Program.cs
+++ Intro/Intro/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Intro.Business;
+using Intro.DataAccess.Abstracts;
+using Intro.DataAccess.Concretes;
 using Intro.Entities;
 
 Console.WriteLine("Hello, World!");
@@ -28,11 +30,45 @@ for (int i = 0; i < loans.Length; i++)
     Console.WriteLine(loans[i]);
 }
 
-CourseManager courseManager = new();
-Course[] courses = courseManager.GetAll();
-for (int i = 0; i < courses.Length; i++)
+CourseManager courseManager = new(new DapperCourseDal());
+List<Course> courses = courseManager.GetAll();
+for (int i = 0; i < courses.Count; i++)
 {
     Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
 }
 
 Console.WriteLine("Kod bitti.");
+
+IndividualCustomer customer1 = new IndividualCustomer();
+customer1.Id = 1;
+customer1.NationalIdentity = "12345678910";
+customer1.FirstName = "Oğuzhan";
+customer1.LastName = "Küreksiz";
+customer1.CustomerNumber = "1234567";
+
+IndividualCustomer customer2 = new IndividualCustomer();
+customer2.Id = 2;
+customer2.NationalIdentity = "12345678911";
+customer2.FirstName = "Alican";
+customer2.LastName = "Küreksiz";
+customer2.CustomerNumber = "1234568";
+
+CorporateCustomer customer3 = new CorporateCustomer();
+customer3.Id = 3;
+customer3.Name = "Kodlamaio";
+customer3.CustomerNumber = "1234569";
+customer3.TaxNumber = "1234562";
+
+CorporateCustomer customer4 = new CorporateCustomer();
+customer4.Id = 4;
+customer4.Name = "TechCareer";
+customer4.CustomerNumber = "1234570";
+customer4.TaxNumber = "1234572";
+
+BaseCustomer[] customers = { customer1,customer2,customer3,customer4};
+
+//polymorphism
+foreach (BaseCustomer customer in customers)
+{
+    Console.WriteLine("Id numarası " + customer.Id + " olan müşterinin müşteri numarası : " + customer.CustomerNumber);
+}
